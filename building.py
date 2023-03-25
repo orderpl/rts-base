62c66b94a466d98a6bfa00fb534bb45b33e1b5be
orderpl <tobiaszperczak0@gmail.com>
2023-03-25 13:19:48 +0100

    fix

--- building.py
+++ building.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame import Surface, Vector2, Rect
 
+debug = True
+
 class Building(object):
     def __init__(self, game, image:Surface, rect:Rect, imageOffset:Vector2, imageSize:Vector2) -> None:
         """Image offset is offset from center"""
@@ -8,9 +10,11 @@ class Building(object):
         self.imageOffset = imageOffset
         self.image:Surface = pygame.transform.scale(image, imageSize)
         self.imageRect:Rect = self.image.get_rect()
-        self.rect:Rect = rect
+        self.rect:Rect = rect # position on the map
         
     def render(self) -> None:
-        renderRect = Rect(self.game.camera.getOnScreenPos(Vector2(self.rect.topleft)) + self.imageOffset, Vector2(self.rect.size) * self.game.camera.zoom)
         renderImage = pygame.transform.scale(self.image, Vector2(self.imageRect.size) * self.game.camera.zoom)
+        renderRect = renderImage.get_rect(center=self.game.camera.getOnScreenPos(Vector2(self.rect.center)) + self.imageOffset)
+        if debug:
+            pygame.draw.rect(self.game.screen, (159, 255, 159), renderRect)
         self.game.screen.blit(renderImage, renderRect)
